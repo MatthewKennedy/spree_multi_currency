41a85ffe45605032fa6188ff8bcfd9c5ef4beb09
Matthew Kennedy <m.kennedy@me.com>
2019-12-20 14:40:09 +0000

    Try

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -9,6 +9,21 @@ module Spree
       if session.key?(:currency) && session[:currency] != current_store.code.upcase
         session[:currency] = current_store.code.upcase
       end
+
+
+      # Keep the store currency and order currency in sync
+      if current_order
+        if current_order.currency != current_currency
+          params[:currency] = current_currency
+        end
+      end
+
+      # Switch the currency based on the params given.
+      if params[:currency].present?
+        @currency = supported_currencies.find { |currency| currency.iso_code == params[:currency] }
+        current_order.update_attributes!(currency: @currency.iso_code) if @currency && current_order
+        session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
+      end
     end
 
   end
